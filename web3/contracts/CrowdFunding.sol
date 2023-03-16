43eb4bc346d075d792b49e7210bb264b1ccbb20b
Muhannad <muhannadqandah1996@gmail.com>
2023-03-16 01:15:36 +0100

    The condition of the require statement in the original code is flipped (deadline should be greater than current timestamp) and it is checking the value of a variable that hasn't been assigned a value yet (0), which which will always be less than the current timestamp. I have flipped the inequality and used the value passed to the function.

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -21,7 +21,7 @@ contract CrowdFunding {
     function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+        require(_deadline > block.timestamp, "The deadline should be a date in the future.");
 
         campaign.owner = _owner;
         campaign.title = _title;
