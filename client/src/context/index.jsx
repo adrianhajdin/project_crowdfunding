5dcf779b7f244400bb4c052d4e7749676d9f772c
Krishna R Acharya <krishna.acharya@radisys.com>
2023-05-28 13:40:41 +0530

    [Resolve issue #31]

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -70,7 +70,7 @@ export const StateContextProvider = ({ children }) => {
 	};
 
 	const donate = async (pId, amount) => {
-		const data = await contract.call("donateToCampaign", pId, {
+		const data = await contract.call("donateToCampaign", [pId], {
 			value: ethers.utils.parseEther(amount),
 		});
 
