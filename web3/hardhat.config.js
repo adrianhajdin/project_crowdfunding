dc059c6f04abbed4f304c197084e29f4cf4840d8
Ashraf <54413503+iamashruu@users.noreply.github.com>
2023-08-25 09:29:45 +0600

    Update hardhat.config.js for Sepolia network
    /** @type import('hardhat/config').HardhatUserConfig */
    module.exports = {
      solidity: {
        version: "0.8.9",
        defaultNetwork: "sepolia",
        networks: {
          hardhat: {},
          sepolia: {
            url: "https://sepolia.rpc.thirdweb.com",
            accounts: [`0x${process.env.PRIVATE_KEY}`],
          },
        },
        settings: {
          optimizer: {
            enabled: true,
            runs: 200,
          },
        },
      },
    };

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -1,14 +1,14 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    version: '0.8.9',
-    defaultNetwork: 'goerli',
+    version: "0.8.9",
+    defaultNetwork: "sepolia",
     networks: {
       hardhat: {},
-      goerli: {
-        url: 'https://rpc.ankr.com/eth_goerli',
-        accounts: [`0x${process.env.PRIVATE_KEY}`]
-      }
+      sepolia: {
+        url: "https://sepolia.rpc.thirdweb.com",
+        accounts: [`0x${process.env.PRIVATE_KEY}`],
+      },
     },
     settings: {
       optimizer: {
