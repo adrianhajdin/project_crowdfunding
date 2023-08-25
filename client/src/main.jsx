63342b9d832f981de419b202d6f33000f360dbf8
Ashraf <54413503+iamashruu@users.noreply.github.com>
2023-08-25 09:26:29 +0600

    Update from goerli to sepolia in main.jsx
    update from goerli to sepolia because georli testnet is not giving eth without ens domain where sepolia is free.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Sepolia } from "@thirdweb-dev/chains";
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import { StateContextProvider } from './context';
@@ -10,7 +11,9 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}
+    activeChain={Sepolia} 
+    clientId="Enter your client ID from thirdweb"> 
     <Router>
       <StateContextProvider>
         <App />
